3a861e17e364e28a868f06333706fe831543b647
Oleg Mikhailov <omikhailov@acest.nl>
2017-01-12 21:30:23 +0200

    first page and page scroll added

---
+++ dev/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    /*
+    * Plugin intialization
+    */
+    $('#pagepiling').pagepiling({
+        direction: 'horizontal',
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+        sectionsColor: ['#000', '#fff', '#9c2856', '#1e272b', '#479c38', '#633978'],
+        navigation: true,
+        afterRender: function(){
+            $('#pp-nav').addClass('custom');
+        },
+        afterLoad: function(anchorLink, index){
+            if(index>1){
+                $('#pp-nav').removeClass('custom');
+            }else{
+                $('#pp-nav').addClass('custom');
+            }
+        }
+    });
+});

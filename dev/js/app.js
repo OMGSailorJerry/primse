d4310ebb2f41ca758a84977162bb9ce7855bdc19
Oleg Mikhailov <omikhailov@acest.nl>
2017-01-24 20:42:47 +0200

    base page paralax test

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
+  
     $('[data-type=sendButton]').on('click', function(){ 
+  
         $('.main__form').addClass('main__form_hide');
+  
         $('.main__form__alert_hide').removeClass('main__form__alert_hide');
+  
+  });
+
+    var rellax = new Rellax('.rellax', {
+
+        // center: true
+
     });
+
 });
 
 function reloadPage() {
+
     location.reload();
+
 }

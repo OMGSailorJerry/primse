0180f65be393a7469b412c5baf639984f281b883
Oleg Mikhailov <omikhailov@acest.nl>
2017-01-25 20:05:16 +0200

    header on scroll animation

--- dev/js/app.js
+++ dev/js/app.js
@@ -14,6 +14,25 @@ $(document).ready(function() {
 
     });
 
+    var $head = $( '#ha-header' );
+
+    $( '.ha-waypoint' ).each( function(i) {
+        var $el = $( this ),
+        
+        animClassDown = $el.data( 'animateDown' ),
+        
+        animClassUp = $el.data( 'animateUp' );
+
+        $el.waypoint( function( direction ) {
+            if( direction === 'down' && animClassDown ) {
+                // debugger
+                $head.attr('class', 'ha-header ' + animClassDown);
+            }
+            else if( direction === 'up' && animClassUp ){
+                $head.attr('class', 'ha-header ' + animClassUp);
+            }
+        }, { offset: '220px' } );
+    } );
 });
 
 function reloadPage() {
